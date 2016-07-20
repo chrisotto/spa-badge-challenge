0d33ece8b5761eb0b27bde0365f8fdf1fc18be51
Chris Otto <email": null>
2016-07-19 20:52:12 -0700

    Add toLowerCase to handlebars

--- app/js/app.js
+++ app/js/app.js
@@ -34,3 +34,7 @@ var renderTemplate = function(view) {
 var getTeachers = function() {
   return {teachers: [{"id":1,"name":"Hunter","created_at":"2016-06-22T09:36:59.818Z","updated_at":"2016-06-22T09:36:59.818Z"},{"id":2,"name":"Derek","created_at":"2016-06-22T09:36:59.830Z","updated_at":"2016-06-22T09:36:59.830Z"},{"id":3,"name":"Seba","created_at":"2016-06-22T09:36:59.837Z","updated_at":"2016-06-22T09:36:59.837Z"},{"id":4,"name":"Shambhavi","created_at":"2016-06-22T09:36:59.843Z","updated_at":"2016-06-22T09:36:59.843Z"},{"id":5,"name":"Ken","created_at":"2016-06-22T09:36:59.848Z","updated_at":"2016-06-22T09:36:59.848Z"},{"id":6,"name":"Walker","created_at":"2016-06-22T09:36:59.854Z","updated_at":"2016-06-22T09:36:59.854Z"},{"id":7,"name":"Julian","created_at":"2016-06-22T09:36:59.860Z","updated_at":"2016-06-22T09:36:59.860Z"},{"id":8,"name":"Jaclyn","created_at":"2016-06-22T09:36:59.866Z","updated_at":"2016-06-22T09:36:59.866Z"},{"id":9,"name":"Jen","created_at":"2016-06-22T09:36:59.871Z","updated_at":"2016-06-22T09:36:59.871Z"},{"id":10,"name":"Sherif","created_at":"2016-06-22T09:36:59.876Z","updated_at":"2016-06-22T09:36:59.876Z"}]};
 };
+
+Handlebars.registerHelper('toLowerCase', function(str) {
+  return str.toLowerCase();
+});
